abd7fadfb6b0ffd6e4fbd106d46642fbdb44844c
lkrath <lkrath@infonline.de>
2020-10-07 18:10:20 +0200

    Stack + Node

---
+++ node.py
@@ -0,0 +1,16 @@
+class Node:
+
+    def __init__(self):
+        self.node = "Node"
+        __content = 0
+        __next_node = 0
+
+    def getContent():
+        return __content
+    
+    def setContent(x):
+        __next_node = __content
+        __content = x
+
+    def getNext():
+        return __next_node
---
+++ stack.py
@@ -0,0 +1,22 @@
+import node
+
+n = node.Node()
+
+class Stack:
+
+
+    def __init__(self):
+        self.stack = "Stack"
+        head = n.getContent()
+
+    def isEmpty():
+        if head == 0:
+            return True
+        else:
+            return False
+
+    def top():
+        return head
+
+    def pop():
+        n.setContent(n.getNext)
