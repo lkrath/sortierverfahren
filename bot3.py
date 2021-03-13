ab7833c0def65d0a123b4a0b54e8af35690b760e
lkrath <lkrath@infonline.de>
2021-03-13 16:55:06 +0100

    bot3

--- bot3.py
+++ bot3.py
@@ -4,7 +4,8 @@ import discord
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
-from youtube_dl import YoutubeDL
+import youtube_dl
+#from youtube_dl import YoutubeDL
 from discord.utils import get
 from discord import FFmpegPCMAudio
 
@@ -18,23 +19,24 @@ bot = commands.Bot(command_prefix="!")
 async def ping(ctx):
     await ctx.channel.send("pong")
 
-@bot.command()
-async def join(ctx):
-    channel = ctx.author.voice.channel
-    #voice = get(bot.voice_clients, guild=ctx.guild)
-    #if not voice.is_connected():
-    await channel.connect()
+#@bot.command()
+#async def join(ctx):
+#    channel = ctx.author.voice.channel
+#    await channel.connect()
 
 @bot.command()
 async def leave(ctx):
-    #channel = ctx.author.voice.channel
-    #if voice.is_connected():
-    #await channel.disconnect()
     server = ctx.message.guild.voice_client
     await server.disconnect()
 
 @bot.command()
+async def join(ctx):
+    vc = discord.utils.get(ctx.guild.voice_channels, name='General')
+    await vc.connect()
+
+@bot.command()
 async def yt(ctx, url:str):
+    print("got song")
     song_there = os.path.isfile("song.mp3")
     try:
         if song_there:
@@ -43,45 +45,16 @@ async def yt(ctx, url:str):
         await ctx.send("Es spielt grade ein Lied.")
         return
         
-        vc = discord.utils.get(ctx.guild.voice_channels, name='General')
-        await vc.connect()
-        voice = discord.utils.get(bot.voice_clients)
-
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192'
-            }],
-        }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-        for file in os.listdir("./"):
-            if file.endswith(".mp3"):
-                os.rename(file, "song.mp3")
-        voice.play(discord.FFmpegPCMAudio("song.mp3"))
-
-@bot.command()
-async def play(ctx, url : str):
-    song_there = os.path.isfile("song.mp3")
-    try:
-        if song_there:
-            os.remove("song.mp3")
-    except PermissionError:
-        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
-        return
-
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
-    await voiceChannel.connect()
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    #vc = discord.utils.get(ctx.guild.voice_channels, name='General')
+    #await vc.connect()
+    voice = discord.utils.get(bot.voice_clients)
 
     ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
-            'preferredquality': '192',
+            'preferredquality': '192'
         }],
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -90,6 +63,23 @@ async def play(ctx, url : str):
         if file.endswith(".mp3"):
             os.rename(file, "song.mp3")
     voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
+@bot.command()
+async def stop(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild = ctx.guild)
+    voice.stop()
+
+@bot.command()
+async def pause(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild = ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+
+@bot.command()
+async def resume(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild = ctx.guild)
+    if voice.is_paused():
+        voice.resume()
 
 @bot.event
 async def on_message(message):
@@ -102,9 +92,6 @@ async def on_message(message):
         if message.author == bot.user:
             return
         await message.channel.send(':D')
-
-    if message.content == 'bonk':
-        await message.channel.send('horny jail')   
 
     if message.content == 'say hi to me':
         await message.author.create_dm()
