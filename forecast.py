88ff896efabb9feaaaa4e13ec378a1c916cc6c42
lkrath <lkrath@infonline.de>
2021-06-27 14:49:03 +0200

    forecat

--- forecast.py
+++ forecast.py
@@ -1,5 +1,6 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import *
+from bs4 import NavigableString, Tag
 
 from app import app
 
@@ -14,16 +15,28 @@ a = "Heute: "
 
 theute = soup.find('div', {'class': 'col-sm-6 tempMaxDiv'})
 
-print(theute.prettify())
+#print(theute.prettify())
 
-temperatur = list(theute.children)[0]
-print(temperatur)
-a += temperatur.get_text() + " "
+#temperatur = list(theute.children)[0]
+#print(temperatur)
+#a += temperatur.get_text() + " "
+
+for body_child in theute.children:
+    if isinstance(body_child, NavigableString):
+        continue
+    if isinstance(body_child, Tag):
+        a += body_child.get_text() + " "
 
 rheute = soup.find('div', {'class', 'col-xs-4'})
 
-regen = list(rheute.children)[0]
-a += "Regenwahrscheinlichkeit: " + regen.get_text() + " "
+#regen = list(rheute.children)[0]
+#a += "Regenwahrscheinlichkeit: " + regen.get_text() + " "
+
+for body_child in rheute.children:
+    if isinstance(body_child, NavigableString):
+        continue
+    if isinstance(body_child, Tag):
+        a +="Regenwahrscheinlichkeit: " + body_child.get_text() + " "
 
 @app.route('/')
 @app.route('/index')
